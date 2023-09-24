741902942be0b611496e3e516fdecd87c7c6d728
qaize <ikhwani.syahbana15@gmail.com>
2023-09-24 10:05:01 +0700

    response util

---
+++ src/main/java/com/example/AppWinterhold/Utility/ResponseUtil.java
@@ -0,0 +1,48 @@
+package com.example.AppWinterhold.Utility;
+
+import com.example.AppWinterhold.Dto.BaseResponseDTO;
+import org.springframework.http.HttpStatus;
+
+public class ResponseUtil {
+
+    public static BaseResponseDTO<Object> insertSuccessResponse(Object data){
+
+        return BaseResponseDTO.<Object>builder()
+                .status("1")
+                .message("Success")
+                .httpStatus(HttpStatus.OK)
+                .data(data)
+                .build();
+    }
+    public static BaseResponseDTO<Object> insertSuccessResponseWithMetaData(Object data, BaseResponseDTO.MetaData metaData){
+
+        return BaseResponseDTO.<Object>builder()
+                .status("1")
+                .message("Success")
+                .httpStatus(HttpStatus.OK)
+                .data(data)
+                .metaData(metaData)
+                .build();
+    }
+
+
+    public static BaseResponseDTO<Object> insertFailResponse(String message){
+
+        return BaseResponseDTO.<Object>builder()
+                .status("0")
+                .message("OK")
+                .httpStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+                .data(message)
+                .build();
+    }
+
+    public static BaseResponseDTO<Object> insertFailResponse(String message, String reason){
+
+        return BaseResponseDTO.<Object>builder()
+                .status("0")
+                .message("OK")
+                .httpStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+                .data(message.concat(reason))
+                .build();
+    }
+}
