e7186316429084eedaf157ddf9ef02ed291d50b6
qaize <ikhwani.syahbana15@gmail.com>
2023-11-02 14:39:16 +0700

    author ajax

--- src/main/resources/static/resources/js/ajax.js
+++ src/main/resources/static/resources/js/ajax.js
@@ -2,14 +2,40 @@ const prevButton = document.getElementById("prev-button");
 const nextButton = document.getElementById("next-button");
 const listAuthor = document.getElementById("table-author");
 const searchAuthor = document.getElementById("search-author");
-const searchButton = document.getElementById("search-button-author");
+const searchButton = document.getElementById("search-button");
+const paginationList = document.getElementById("pagination-list");
+const paginationCurrent = document.getElementById("pagination-current");
+const paging = document.getElementById("paging");
 
 let totalPage = 0;
 
 var page = 1;
 var nama = "";
 
+// let role = document.querySelector(".role");
+// if (role != null) {
+//   let roleName = role.textContent;
+//   let formatted = roleName
+//     .replace("[", "")
+//     .replace("]", "")
+//     .replace("ROLE_", "");
+//   role.textContent = formatted;
+// } else {
+//   let mainBody = document.querySelector(".main-body");
+//   let roleName = mainBody.getAttribute("data-role");
+//   let formatted = roleName
+//     .replace("[", "")
+//     .replace("]", "")
+//     .replace("ROLE_", "");
+//   mainBody.setAttribute("data-role", formatted);
+// }
+
 searchAuthor.addEventListener("keyup", () => {
+  nama = searchAuthor.value;
+  loadAuthorData();
+});
+
+searchButton.addEventListener("click", () => {
   nama = searchAuthor.value;
   loadAuthorData();
 });
@@ -35,28 +61,60 @@ function loadAuthorData() {
     type: "GET",
     success: function (user) {
       totalPage = user.metaData.totalPage;
-      console.log(user);
+      paging.style.display = "flex";
+      paginationList.innerHTML = "";
+      paginationCurrent.innerHTML = `page ${page} of ${totalPage}`;
+
+      // if true pagination not showing
+      if (totalPage <= 1) {
+        paging.style.display = "none";
+      }
+
+      // Generating Button lists
+      for (let i = 0; i < totalPage; i++) {
+        const button = document.createElement("button");
+        button.innerHTML = 1 + i;
+
+        button.addEventListener("click", () => {
+          page = i + 1;
+          loadAuthorData();
+        });
+
+        paginationList.appendChild(button);
+      }
+
+      // insert data to table with Next and prev paging
       if (user.metaData.totalPage >= page) {
         listAuthor.innerHTML = "";
         $.each(user.data, function (key, value) {
-          $("#table-author").append(`
-          <tr>
-            <td>
-                <div class="button-wrapper">
-                    <a class="btn blue-button" href="/winterhold/author/detail?id=${value.id}">Books</a>
-                    <a class="btn update-button" href="/winterhold/author/update?id=${value.id}"">Edit</a>
-                    <a class="btn warning-button" href="/winterhold/author/delete?id=${value.id}"">Delete</a>
-                </div>
-            </td>
-            <td>${value.fullname}</td>
-            <td>${value.age}</td>
-            <td>${value.deceasedDateStr}</td>
-            <td>${value.education}</td>
-            <td>${value.createdBy}</td>
-            <td>${value.modifiedBy}</td>
-          </tr>
+          let adminAction = ``;
 
-          `);
+          if (role.textContent === "administrator") {
+            adminAction = `
+            <a class="btn update-button" href="/winterhold/author/update?id=${value.id}">Edit</a>
+            <a class="btn warning-button" href="/winterhold/author/delete?id=${value.id}">Delete</a>`;
+          }
+
+          $("#table-author").append(
+            `
+            <tr>
+              <td>
+                  <div class="button-wrapper">
+                      <a class="btn blue-button" href="/winterhold/author/detail?id=${value.id}">Books</a>` +
+              adminAction +
+              `
+                  </div>
+              </td>
+              <td>${value.fullname}</td>
+              <td>${value.age}</td>
+              <td>${value.deceasedDateStr}</td>
+              <td>${value.education}</td>
+              <td>${value.createdBy}</td>
+              <td>${value.modifiedBy}</td>
+            </tr>
+
+            `
+          );
         });
       }
     },
